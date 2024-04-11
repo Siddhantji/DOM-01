2dc078f9293f15fec5704e76750f8466d458ab09
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-11 20:50:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const count = document.getElementById("count");
+const err = document.getElementById("err");
+const inc = document.getElementById("inc");
+const dec = document.getElementById("dec");
+const clear = document.getElementById("clear");
+
+let a = 0;
+function display(){
+    count.textContent=a;
+    if (a===0){
+        err.style.display="block";
+        clear.style.display="none";
+        dec.disabled = true;
+
+    }
+    else{
+        err.style.display="none";
+        clear.style.display="inline";
+        dec.disabled = false;
+    }
+    
+}
+inc.addEventListener("click",function(){
+    a++;
+    display();
+})
+dec.addEventListener("click",function(){
+    a--;
+    display();
+})
+clear.addEventListener("click",function(){
+    a=0;
+    display();
+})
